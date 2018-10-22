25127c82da3cf4101ba9adb69ccc6fa3c289ab8f
tonynguyen111997 <tonynguyen111997@gmail.com>
2018-10-22 21:39:27 +0100

    newRegister

---
+++ app/src/main/java/com/example/android/socialmediaproject/register_birthday.java
@@ -0,0 +1,13 @@
+package com.example.android.socialmediaproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class register_birthday extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_birthday);
+    }
+}
